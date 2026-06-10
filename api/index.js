7eaf92ede8b43986786f451720c0831517b8ae9f
Naseem Butt <naseembuttnova@gmail.com>
2026-06-10 14:53:19 -0700

    fix cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const serverless = require("serverless-http");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/products", (req, res) => {
