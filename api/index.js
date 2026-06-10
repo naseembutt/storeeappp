4f03d8ded6870a5c3ec61584b853e2e745c8c902
Naseem Butt <naseembuttnova@gmail.com>
2026-06-10 14:30:57 -0700

    fix api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const serverless = require("serverless-http");
+
 const app = express();
 
 app.use(express.json());
 
 app.get("/products", (req, res) => {
   res.json({ success: true, data: [] });
+});
+
+app.post("/products", (req, res) => {
+  res.status(201).json({
+    success: true,
+    data: req.body,
+  });
 });
 
 module.exports = serverless(app);
