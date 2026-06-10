0af3699bee0545d0d1ddd50a01184c093eabfaba
Naseem Butt <naseembuttnova@gmail.com>
2026-06-10 11:54:52 -0700

    final fix

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"; // 1. Ye add kiya
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import productRoute from "./routes/product.route.js";
 import path from "path";
@@ -14,10 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-// 2. CORS allow karna zaroori hai taake frontend connect ho sakay
 app.use(
   cors({
-    origin: "http://localhost:5175", // Aapka frontend is port par chal raha hai
+    origin: "http://localhost:5175",
     credentials: true,
   }),
 );
@@ -30,13 +29,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
-    // 3. Is line ko theek kiya (ye "*" hota hai)
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
-
-app.listen(PORT, () => {
-  // 4. Isay 5000 ke bajaye PORT variable kiya
-  connectDB();
-  console.log("Server started at http://localhost:" + PORT);
-});
+export default app;
